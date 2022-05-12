72542d917d4ec8fbd0d85e0fb9c1473ab351c134
user <user@DESKTOP-B5S27PK>
2022-05-12 14:31:59 -0700

    Add project files.

---
+++ Delegates/Class2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Delegates.MyClass;
+
+namespace Delegates
+{
+    public delegate void Funct(string str);
+
+    public class MyClass
+    {
+      
+        public MyClass(string s)
+        {
+           
+        }
+
+        public void Space(string str)
+        {
+            for (int i = 1; i < str.Length; i += 2)
+            {
+                str = str.Insert(i, "_");
+            }
+            Console.WriteLine(str);
+        }
+
+        public void Reverse(string str)
+        {
+            string s = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                s += str[i];
+            }
+            Console.WriteLine(s);
+        }
+    }
+    class Run
+    {
+        public void runFunc(Funct delg, string str)
+        {
+            delg.Invoke(str);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,13 @@
+﻿using Delegates;
+using System;
+
+Console.Write("Enter string: ");
+var str = Console.ReadLine();
+
+MyClass myClass = new(str);
+
+Funct fun = myClass.Space;
+fun += myClass.Reverse;
+
+Run run = new();
+run.runFunc(fun, str);
